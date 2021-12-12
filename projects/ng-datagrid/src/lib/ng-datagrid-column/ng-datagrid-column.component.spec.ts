ce27467bfae3f141a91a7f489e45543571ba1ff3
yusufcmt <yusuf.comert@sono.com.tr>
2021-12-12 19:19:49 +0300

    @angular/core migration - migration-v11-wait-for-async
    `async` to `waitForAsync` migration. The `async` testing function has been renamed to `waitForAsync` to avoid confusion with the native `async` keyword.

--- projects/ng-datagrid/src/lib/ng-datagrid-column/ng-datagrid-column.component.spec.ts
+++ projects/ng-datagrid/src/lib/ng-datagrid-column/ng-datagrid-column.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgDatagridColumnComponent } from './ng-datagrid-column.component';
 
@@ -6,7 +6,7 @@ describe('NgDatagridColumnComponent', () => {
   let component: NgDatagridColumnComponent;
   let fixture: ComponentFixture<NgDatagridColumnComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgDatagridColumnComponent ]
     })
